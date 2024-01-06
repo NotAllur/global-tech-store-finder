e08d6989db5eb233c118069d12f53f3e3f1642e3
Adam <94263572+NotAllur@users.noreply.github.com>
2024-01-06 01:56:01 +0100

    Website's creation

---
+++ js/lightSwitch.js
@@ -0,0 +1,47 @@
+function lightSwitch() {
+  const themeLink = document.getElementById("theme");
+  const lightSwitch = document.getElementById("lightSwitch");
+
+  if (themeLink.getAttribute("href") === "../css/dark.css") {
+    setTheme("light", themeLink, lightSwitch);
+  } else {
+    setTheme("dark", themeLink, lightSwitch);
+  }
+}
+
+function setTheme(theme, themeLink, lightSwitch) {
+  if (theme === "light") {
+    themeLink.setAttribute("href", "../css/light.css");
+    lightSwitch.src = "../media/moon-svgrepo-com.svg";
+    localStorage.setItem("theme", "light");
+  } else {
+    themeLink.setAttribute("href", "../css/dark.css");
+    lightSwitch.src = "../media/day-sunny-svgrepo-com.svg";
+    localStorage.setItem("theme", "dark");
+  }
+}
+
+function loadTheme() {
+  const themePreference = localStorage.getItem("theme");
+
+  if (
+    themePreference === "light" ||
+    (themePreference === null &&
+      window.matchMedia("(prefers-color-scheme: light)").matches)
+  ) {
+    setTheme(
+      "light",
+      document.getElementById("theme"),
+      document.getElementById("lightSwitch")
+    );
+  } else {
+    setTheme(
+      "dark",
+      document.getElementById("theme"),
+      document.getElementById("lightSwitch")
+    );
+  }
+}
+
+document.getElementById("lightSwitch").addEventListener("click", lightSwitch);
+window.onload = loadTheme;
---
+++ js/searchBar.js
@@ -0,0 +1,19 @@
+const searchInput = document.getElementById("search");
+searchInput.value = ""; // Clear search input value
+
+// Implement search functionality
+searchInput.addEventListener("input", function () {
+  let searchText = this.value.toLowerCase();
+  let countryNames = document.getElementsByClassName("countryName");
+
+  Array.from(countryNames).forEach(function (country) {
+    let name = country.textContent.toLowerCase();
+    let row = country.parentElement;
+
+    if (name.includes(searchText)) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+});
